d5102202fc19756bb71e8b4ce7b6424f29fcaefa
rizikolik <ynsture@gmail.com>
2019-07-31 23:48:28 +0300

    handled the marker

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,15 +1,20 @@
 import React from "react";
+import marked from "marked";
 
 class Editor extends React.Component{
     constructor(props){
         super(props);
+        this.handleChange=this.handleChange.bind(this)
     }
-
+handleChange(e){
+    let preview=document.getElementById("preview");
+    preview.innerHTML=marked(e.target.value)
+}
 
 render(){
     return(
 <div className="editor-wrapper">
-<textarea  id="editor">
+<textarea  id="editor" onChange={this.handleChange}>
 Hello this is init
 </textarea>
 
